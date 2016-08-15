1fd1ef37f309649f7385d8cebe5285b7edfcc58d
legend <499458212@qq.com>
2016-08-15 11:06:58 +0800

    Initial Commit

---
+++ SQLCipherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQLCipherApp
+//
+//  Created by 张传奇 on 16/8/15.
+//  Copyright © 2016年 张传奇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SQLCipherApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SQLCipherApp
+//
+//  Created by 张传奇 on 16/8/15.
+//  Copyright © 2016年 张传奇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
